70c77c12aecc75cd0c0436a7ec95dbae27951ddf
Tirsa St. Fort <lifelovelori@gmail.com>
2023-04-07 11:02:22 -0700

    Add padding and border to table

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch("http://localhost:3000/lifters")
         .then((response) => response.json())
         .then((data) => {
-            //const table = document.querySelector("tbody")
             for (let lifter of data) {
                 addLifterStats(lifter)
             }
